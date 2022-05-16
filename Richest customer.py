490dd90aedc7b5e78d85f91941eaa2c90a6e0972
Bagavathi Priya Naveneethakrishnan <61631098+bagavathypriyanavaneethan@users.noreply.github.com>
2022-05-16 08:52:32 +0530

    Delete Richest customer.py

--- Richest customer.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        
-        return max(map(sum,accounts))
-    
-    #map(sum,accounts) gives the sum of the wealth for each customers 
-    #max gives the maximum among those wealths
