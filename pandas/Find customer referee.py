efa852be600b8d21387dd145935091fc75197676
Bagavathi Priya Naveneethakrishnan <61631098+bagavathypriyanavaneethan@users.noreply.github.com>
2023-12-13 16:24:23 +0530

    https://leetcode.com/problems/find-customer-referee

---
+++ pandas/Find customer referee.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+
+    
+    result = customer[(customer['referee_id'] != 2) | (customer['referee_id'].isna())]
+
+    # result = result['name']
+    
+    return result[['name']]
+    
